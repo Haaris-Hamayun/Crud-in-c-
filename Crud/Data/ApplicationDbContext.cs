8caebbf46d11f39ca7c84f9f9967a8dc5f9e950b
Haris hamayun <haris1122hamayun@gmail.com>
2025-07-18 23:18:25 +0500

    Add project files.

---
+++ Crud/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Crud.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Crud.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options) : base(options)
+        {
+            
+        }
+        public DbSet<Students> Students { get; set; }
+
+
+    }
+}
---
+++ Crud/Models/Students.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Crud.Models
+{
+    public class Students { 
+
+
+        [Key]
+    [Display(Name = "Student ID")]
+    public int StudentID { get; set; }
+
+        [Required]
+        [Display(Name = "Student Name")]
+        public string Name  { get; set; }
+
+        public string Email { get; set; }
+
+        public string Course { get; set; }
+
+        
+        [Display(Name = "EnrollementDate")]
+
+        public DateTime EnrollmentDate { get; set; }
+
+    }
+}
